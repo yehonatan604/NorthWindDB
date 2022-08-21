c233dcf06242950b1d2e451f0493fdd5a30ccc50
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-08-21 17:33:02 +0300

    final

--- NorthWindDB.WpfApp/MainWindow.xaml.cs
+++ NorthWindDB.WpfApp/MainWindow.xaml.cs
@@ -75,13 +75,11 @@ namespace NorthWindDB.WpfApp
 
         private void Btn6_Click(object sender, RoutedEventArgs e)
         {
-            OrderDetail ord1 = new() { ProductId = 11, UnitPrice = 95, Quantity = 3 };
-            OrderDetail ord2 = new() { ProductId = 56, UnitPrice = 47, Quantity = 6 };
-            OrderDetail ord3 = new() { ProductId = 74, UnitPrice = 9120, Quantity = 5 };
+            int order = nw.Orders.Skip(nw.Orders.Count() - 1).FirstOrDefault().OrderId;
 
-            nw.OrderDetails.Add(ord1);
-            nw.OrderDetails.Add(ord2);
-            nw.OrderDetails.Add(ord3);
+            OrderDetail ord1 = new() { ProductId = 11, OrderId = order, UnitPrice = 95, Quantity = 3, Discount = 0 };
+            OrderDetail ord2 = new() { ProductId = 56, OrderId = order, UnitPrice = 47, Quantity = 6, Discount = 0 };
+            OrderDetail ord3 = new() { ProductId = 74, OrderId = order, UnitPrice = 120, Quantity = 5, Discount = 0 };
 
             nw.Orders.Add(new Order() { CustomerId = "FRANK",
                 EmployeeId = 6,
@@ -89,12 +87,18 @@ namespace NorthWindDB.WpfApp
                 ShipAddress = "7 Piccadilly Rd.",
                 ShipCity = "New York",
                 ShipCountry = "New York",
-                OrderDetails = new List<OrderDetail>() { ord1, ord2, ord3 } 
             });
 
             nw.SaveChanges();
 
-            var query = from order in nw.Orders select order;
+            nw.OrderDetails.Add(ord1);
+            nw.OrderDetails.Add(ord2);
+            nw.OrderDetails.Add(ord3);
+
+            nw.SaveChanges();
+
+            var query = from ord in nw.OrderDetails 
+                        select new { ord.OrderId, ord.ProductId, ord.UnitPrice, ord.Quantity, ord.Discount };
 
             Dgrid_Update(query.ToList());
         }
@@ -115,21 +119,23 @@ namespace NorthWindDB.WpfApp
 
         private void Btn8_Click(object sender, RoutedEventArgs e)
         {
-            var record = nw.Orders.SingleOrDefault(x => x.OrderId == 11085);
+            int order = nw.Orders.Skip(nw.Orders.Count() - 1).FirstOrDefault().OrderId;
+            var record = nw.Orders.SingleOrDefault(x => x.OrderId == order);
             record.EmployeeId = 5;
 
-            var query = from order in nw.Orders
-                        where order.OrderId == 11085
-                        select order;
+            var query = from ord in nw.Orders
+                        where ord.OrderId == order
+                        select ord;
 
             Dgrid_Update(query.ToList());
         }
 
         private void Btn9_Click(object sender, RoutedEventArgs e)
         {
-            nw.Orders.Remove(nw.Orders.SingleOrDefault(x => x.OrderId == 11084));
+            int order = nw.OrderDetails.Skip(nw.OrderDetails.Count() - 1).FirstOrDefault().OrderId;
+            nw.OrderDetails.Remove(nw.OrderDetails.Single(x => x.ProductId == 56 && x.OrderId == order));
 
-            var query = from order in nw.Orders select order;
+            var query = from item in nw.OrderDetails select item;
 
             Dgrid_Update(query.ToList());
         }
